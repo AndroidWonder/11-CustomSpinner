2c047453a5a2ba78a78ff491043b1454454cebe7
ClassroomAccount <jpepe44@gmail.com>
2020-04-30 16:34:27 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/customspinner/MainActivity.java
@@ -0,0 +1,37 @@
+package com.course.example.customspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    String[] countryNames={"Denmark","China","Germany","UK","India","Canada"};
+    int flags[] = {R.drawable.denmark, R.drawable.china, R.drawable.germany, R.drawable.uk, R.drawable.india, R.drawable.ca};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.simpleSpinner);
+        spin.setOnItemSelectedListener(this);
+
+        CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),flags,countryNames);
+        spin.setAdapter(customAdapter);
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), countryNames[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+}
